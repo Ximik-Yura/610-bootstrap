da516fab8a668d432359a4205fa1765781d2e397
Ximik-Yura <chimikur4uk@gmail.com>
2021-10-02 17:22:26 +0000

    Some js code

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const navBarBrand = document.querySelector(".navbar-brand");
+navBarBrand.innerText +='!';
+
+function updateClock(){
+    const clock = document.querySelector('.clock');
+    clock.innerText = (new Date()).toLocaleTimeString();
+}
+setInterval(updateClock, 1000);
